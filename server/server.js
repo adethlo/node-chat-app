48f6ae34cedcd1018f0b0365a6ff57e9394bc72f
Alexander Dethloff <alexander_dethloff@Alexanders-MBP.fritz.box>
2018-09-20 22:02:02 +0200

    added socket.io

--- server/server.js
+++ server/server.js
@@ -1,12 +1,28 @@
+// Load required libraries
 const express = require('express');
 const path = require('path');
+// Load libraries for socket.io
+const http = require('http');
+const socketIO = require('socket.io');
 
 let app = express();
+// Important for socket.io
+let server = http.createServer(app); 
+let io = socketIO(server);
+
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+    console.log('New user connected');
+
+    socket.on('disconnect', (socket) => {
+        console.log('User was disconnected');
+    });
+});
+
+server.listen(port, () => { // Use server instead of app for socket.io to work
     console.log(`Server is listening to port ${port}`);
 });
