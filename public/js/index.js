089d4b0a172c608f6cfc2ddb74efca29236574ef
Alexander Dethloff <alexander_dethloff@Alexanders-MBP.fritz.box>
2018-09-22 12:54:36 +0200

    Added scroll functionality

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,21 @@
 let socket = io();
 
+function scrollToBottom () {
+    // Selectors
+    let messages = jQuery('#messages');
+    newMessage = messages.children('li:last-child');
+    // Heights
+    let clientHeight = messages.prop('clientHeight');
+    let scrollTop = messages.prop('scrollTop');
+    let scrollHeight = messages.prop('scrollHeight');
+    let newMessageHeight = newMessage.innerHeight();
+    let lastMessageHeight = newMessage.prev().innerHeight();
+
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        messages.scrollTop(scrollHeight);
+    }
+}
+
 socket.on('connect', function () {
     console.log('Connected to the server');
 });
@@ -17,6 +33,7 @@ socket.on('newMessage', function(message) {
         created_at: formattedTime
     });
     jQuery('#messages').append(html);
+    scrollToBottom();
 });
 
 socket.on('newLocationMessage', function(message) {
@@ -28,6 +45,7 @@ socket.on('newLocationMessage', function(message) {
         created_at: formattedTime
     });
     jQuery('#messages').append(html);
+    scrollToBottom();
 });
 
 jQuery('#message-form').on('submit', function(e) {
